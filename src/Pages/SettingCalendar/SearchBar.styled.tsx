a3b9351ede4ac5e8d168ff85fefd0a8b993dfc13
TangJetsada123 <Jetsada_Jinaka@hotmail.com>
2022-07-14 16:02:57 +0700

    create Setting date

--- src/Pages/SettingCalendar/SearchBar.styled.tsx
+++ src/Pages/SettingCalendar/SearchBar.styled.tsx
@@ -1,33 +1,104 @@
 import styled from "styled-components";
 
 export const ButtonSave = styled.button`
-    padding:2%;
-    width:  25%;
-    background-color: #F57F17;
-    text-align: center;
-    border-radius: 15px;
-    border: none ;
-    margin-top: 3%;
-    margin-bottom: 3% ;
-
-`
+  padding-right: 40%;
+  padding-left: 40%;
+  padding-top: 6%;
+  padding-bottom: 6%;
+  display: inline-block;
+  flex-direction: row;
+  justify-content: center;
+  white-space: nowrap;
+  background-color: #f57f17;
+  text-align: center;
+  border-radius: 15px;
+  border: none;
+  margin-left: 300%;
+  margin-left: 300%;
+  margin-top: 10%;
+  margin-bottom: 10%;
+  color: #fcfcfc; ;
+`;
 export const NavBarSt = styled.div`
-    display: flex ;
-    
-    background: #FFFFFF;
-    box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.25);
-    
-`
+  display: flex;
+  background: #ffffff;
+  box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.25);
+`;
 
-export const Container = styled.div`
-    margin-left: 20vh;
-    margin-right: 20vh;
-    align-items: center;
-    display: flex;
-    justify-content: space-between;
-    font-family: 'Poppins', sans-serif;
-`
+export const NavButton = styled.div`
+  margin-left: 10%;
+  align-items: center;
+  display: flex;
+  gap: 450%;
+  justify-content: space-between;
+  font-family: "Poppins", sans-serif;
+`;
 export const BackButton = styled.button`
-    border: none;
+  border: none;
+  margin-right: 300%;
+  color: #f57f17;
+  background-color: #ffffff;
+`;
 
+export const SettingTable = styled.div`
+  margin-left: 25%;
+  margin-top: 10%;
+`;
+
+export const Table = styled.table`
+  width: 70%;
+  border: 2px solid #f57f17;
+  border-collapse: collapse;
+`;
+
+export const TableHead = styled.tr`
+  background-color: #f57f17;
+  
+`;
+
+export const Head1 = styled.th``;
+
+export const Head2 = styled.th``;
+
+export const SettingDate = styled.td`
+  display: inline;
+  gap: 50px;
+`;
+export const Setting1 = styled.tr`
+  display: flex;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  margin-left: 15%;
+`;
+export const Setting2 = styled.tr`
+  display: flex;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  margin-left: 15%;
+`;
+export const Setting3 = styled.tr`
+  display: flex;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  margin-left: 15%;
+`;
+export const Setting4 = styled.tr`
+  display: flex;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  margin-left: 15%;
+`;
+export const Setting5 = styled.tr`
+  display: flex;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  margin-left: 15%;
+`;
+export const Setting6 = styled.tr`
+  display: flex;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  margin-left: 15%;
+`;
+export const SettingName = styled.td`
 `
--- src/Pages/SettingCalendar/SearchBar.tsx
+++ src/Pages/SettingCalendar/SearchBar.tsx
@@ -1,19 +1,90 @@
 import React from "react";
 import {
-  Container,
+  NavButton,
   NavBarSt,
   ButtonSave,
-  BackButton
+  BackButton,
+  SettingTable,
+  Table,
+  TableHead,
+  Head1,
+  Head2,
+  SettingDate,
+  Setting1,
+  Setting2,
+  Setting3,
+  Setting4,
+  Setting5,
+  Setting6,
+  SettingName
 } from "./SearchBar.styled";
 
 const SearchBar: React.FC = () => {
   return (
-    <NavBarSt>
-      <Container>
-        <BackButton> Back</BackButton>
-        <ButtonSave>saveChange</ButtonSave>
-      </Container>
-    </NavBarSt>
+    <div>
+      <NavBarSt>
+        <NavButton>
+          <BackButton> Back</BackButton>
+          <ButtonSave>Save Changes</ButtonSave>
+        </NavButton>
+      </NavBarSt>
+      <SettingTable>
+        <Table>
+          <TableHead>
+            <Head1>ชื่อวันหยุด</Head1>
+            <Head2>วันที่เริ่มวันหยุด</Head2>
+          </TableHead>
+          <Setting1>
+            <SettingName>Alfreds Futterkiste</SettingName>
+            <SettingDate>
+              <span>1</span>
+              <span>มกราคม</span>
+              <span>1 วัน </span>
+            </SettingDate>
+          </Setting1>
+          <Setting2>
+          <td>Alfreds Futterkiste</td>
+          <SettingDate>
+              <span>1</span>
+              <span>มกราคม </span>
+              <span>1 วัน </span>
+            </SettingDate>
+          </Setting2>
+          <Setting3>
+          <td>Alfreds Futterkiste</td>
+          <SettingDate>
+              <span>Maria Anders</span>
+              <span>Maria Anders</span>
+              <span>Maria Anders</span>
+            </SettingDate>
+          </Setting3>
+          <Setting4>
+          <td>Alfreds Futterkiste</td>
+          <SettingDate>
+              <span>Maria Anders</span>
+              <span>Maria Anders</span>
+              <span>Maria Anders</span>
+            </SettingDate>
+          </Setting4>
+          <Setting5>
+          <td>Alfreds Futterkiste</td>
+          <SettingDate>
+              <span>Maria Anders</span>
+              <span>Maria Anders</span>
+              <span>Maria Anders</span>
+            </SettingDate>
+          </Setting5>
+          <Setting6>
+          <td>Alfreds Futterkiste</td>
+          <SettingDate>
+              <span>Maria Anders</span>
+              <span>Maria Anders</span>
+              <span>Maria Anders</span>
+            </SettingDate>
+          </Setting6>
+        </Table>
+      </SettingTable>
+    </div>
   );
 };
 
