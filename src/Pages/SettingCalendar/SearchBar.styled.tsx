501f0290fc341cc3b2d2ede9ac40481e93145860
TangJetsada123 <Jetsada_Jinaka@hotmail.com>
2022-07-15 19:44:47 +0700

    fix

--- src/Pages/SettingCalendar/SearchBar.styled.tsx
+++ src/Pages/SettingCalendar/SearchBar.styled.tsx
@@ -40,65 +40,170 @@ export const BackButton = styled.button`
   background-color: #ffffff;
 `;
 
+export const HeadName = styled.div`
+  margin-top: 3%;
+  margin-left: 10%;
+  display: flex;
+`;
+export const CalendarIcon = styled.div`
+  color: #f57f17;
+`;
+
+export const HeadTag = styled.div``;
+
+export const HeadName2 = styled.p`
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 28px;
+  color: #f57f17;
+  padding: 0;
+  margin: 0;
+`;
+
+export const HeadName3 = styled.p`
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+  color: rgba(0, 0, 0, 0.85);
+  padding: 0;
+  margin: 0;
+`;
+
 export const SettingTable = styled.div`
-  margin-left: 25%;
-  margin-top: 10%;
+  margin-top: 3%;
 `;
 
 export const Table = styled.table`
-  width: 70%;
-  border: 2px solid #f57f17;
+  width: 50%;
+  margin-left: 25%;
+  border: 2px 0 solid #f57f17;
   border-collapse: collapse;
 `;
 
 export const TableHead = styled.tr`
+  text-align: center;
   background-color: #f57f17;
-  
+  color: #ffffff;
 `;
 
-export const Head1 = styled.th``;
-
-export const Head2 = styled.th``;
-
-export const SettingDate = styled.td`
-  display: inline;
-  gap: 50px;
+export const Head1 = styled.th`
+  padding-top: 1%;
+  padding-bottom: 1%;
 `;
+
+export const Head2 = styled.th`
+  text-align: center;
+`;
+
+export const SettingDate = styled.td``;
 export const Setting1 = styled.tr`
-  display: flex;
-  margin-top: 10px;
-  margin-bottom: 10px;
-  margin-left: 15%;
+  border: 2px solid #f57f17;
+  border-collapse: collapse;
 `;
 export const Setting2 = styled.tr`
-  display: flex;
-  margin-top: 10px;
-  margin-bottom: 10px;
-  margin-left: 15%;
+  border: 2px solid #f57f17;
+  border-collapse: collapse;
 `;
 export const Setting3 = styled.tr`
-  display: flex;
-  margin-top: 10px;
-  margin-bottom: 10px;
-  margin-left: 15%;
+  border: 2px solid #f57f17;
+  border-collapse: collapse;
 `;
 export const Setting4 = styled.tr`
-  display: flex;
-  margin-top: 10px;
-  margin-bottom: 10px;
-  margin-left: 15%;
+  border: 2px solid #f57f17;
+  border-collapse: collapse;
 `;
 export const Setting5 = styled.tr`
-  display: flex;
-  margin-top: 10px;
-  margin-bottom: 10px;
-  margin-left: 15%;
+  border: 2px solid #f57f17;
+  border-collapse: collapse;
 `;
 export const Setting6 = styled.tr`
-  display: flex;
-  margin-top: 10px;
-  margin-bottom: 10px;
-  margin-left: 15%;
+  border: 2px solid #f57f17;
+  border-collapse: collapse;
 `;
-export const SettingName = styled.td`
+export const Setting7 = styled.tr`
+  border: 2px solid #f57f17;
+  border-collapse: collapse;
+`;
+export const Setting8 = styled.tr`
+  border: 2px solid #f57f17;
+  border-collapse: collapse;
+`;
+export const Setting9 = styled.tr`
+  border: 2px solid #f57f17;
+  border-collapse: collapse;
+`;
+export const Setting10 = styled.tr`
+  border: 2px solid #f57f17;
+  border-collapse: collapse;
+`;
+
+export const SettingDay = styled.div`
+  position: absolute;
+  align-items: center;
 `
+
+export const SettingName = styled.td`
+  text-align: left;
+  padding-top: 2%;
+  padding-bottom: 1%;
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 19px;
+  /* identical to box height */
+
+  color: #000000;
+`;
+
+export const AdjustDate = styled.div`
+  display: flex;
+  gap: 50px;
+  margin-left: 10%;
+`;
+export const AdjustButton = styled.button`
+  border: none;
+  background-color: #ffffff;
+`;
+
+export const ButtonChange = styled.div`
+  position: absolute;
+  right: 25%;
+  margin-top: 1%;
+`;
+
+export const AdvanceSelet = styled.div`
+  position: absolute;
+  margin-top: 1%;
+`;
+export const ButtonSelect = styled.button`
+  border: none;
+  background-color: #ffffff;
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 16px;
+
+  /* Text/ Text50 */
+
+  color: rgba(0, 0, 0, 0.5);
+`;
+
+export const AdvanceButton = styled.button`
+  border: none;
+  background-color: #ffffff;
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 16px;
+
+  /* Text /Text85 */
+
+  color: rgba(0, 0, 0, 0.85);
+`;
--- src/Pages/SettingCalendar/SearchBar.tsx
+++ src/Pages/SettingCalendar/SearchBar.tsx
@@ -1,3 +1,5 @@
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 import React from "react";
 import {
   NavButton,
@@ -16,7 +18,23 @@ import {
   Setting4,
   Setting5,
   Setting6,
-  SettingName
+  Setting7,
+  Setting8,
+  Setting9,
+  Setting10,
+  SettingName,
+  AdjustDate,
+  ButtonSelect,
+  ButtonChange,
+  HeadName,
+  HeadName2,
+  HeadName3,
+  AdvanceButton,
+  AdvanceSelet,
+  AdjustButton,
+  HeadTag,
+  CalendarIcon,
+  SettingDay,
 } from "./SearchBar.styled";
 
 const SearchBar: React.FC = () => {
@@ -28,6 +46,15 @@ const SearchBar: React.FC = () => {
           <ButtonSave>Save Changes</ButtonSave>
         </NavButton>
       </NavBarSt>
+      <HeadName>
+        <CalendarIcon >
+        <CalendarTodayIcon />
+        </CalendarIcon>
+        <HeadTag>
+          <HeadName2>Holiday Settings</HeadName2>
+          <HeadName3>วันหยุดราชการ/ วันหยุดราชกิจจานุเบกษา</HeadName3>
+        </HeadTag>
+      </HeadName>
       <SettingTable>
         <Table>
           <TableHead>
@@ -35,53 +62,145 @@ const SearchBar: React.FC = () => {
             <Head2>วันที่เริ่มวันหยุด</Head2>
           </TableHead>
           <Setting1>
-            <SettingName>Alfreds Futterkiste</SettingName>
+            <SettingDay>
+            <SettingName>วันขึ้นปีใหม่</SettingName>
+            </SettingDay>
             <SettingDate>
-              <span>1</span>
-              <span>มกราคม</span>
-              <span>1 วัน </span>
+              <AdjustDate>
+                <span>1</span>
+                <span>มกราคม</span>
+                <span>1 วัน </span>
+                <AdjustButton>
+                  <MoreVertIcon />
+                </AdjustButton>
+              </AdjustDate>
             </SettingDate>
           </Setting1>
           <Setting2>
-          <td>Alfreds Futterkiste</td>
-          <SettingDate>
-              <span>1</span>
-              <span>มกราคม </span>
-              <span>1 วัน </span>
+            <SettingName> วันจักรี</SettingName>
+            <SettingDate>
+              <AdjustDate>
+                <span>1</span>
+                <span>มกราคม </span>
+                <span>1 วัน </span>
+                <AdjustButton>
+                  <MoreVertIcon />
+                </AdjustButton>
+              </AdjustDate>
             </SettingDate>
           </Setting2>
           <Setting3>
-          <td>Alfreds Futterkiste</td>
-          <SettingDate>
-              <span>Maria Anders</span>
-              <span>Maria Anders</span>
-              <span>Maria Anders</span>
+            <SettingName> วันสงกรานต์</SettingName>
+            <SettingDate>
+              <AdjustDate>
+                <span>1</span>
+                <span>มกราคม</span>
+                <span>1 วัน </span>
+                <AdjustButton>
+                  <MoreVertIcon />
+                </AdjustButton>
+              </AdjustDate>
             </SettingDate>
           </Setting3>
           <Setting4>
-          <td>Alfreds Futterkiste</td>
-          <SettingDate>
-              <span>Maria Anders</span>
-              <span>Maria Anders</span>
-              <span>Maria Anders</span>
+            <SettingName> วันฉัตรมงคล</SettingName>
+            <SettingDate>
+              <AdjustDate>
+                <span>1</span>
+                <span>มกราคม</span>
+                <span>1 วัน </span>
+                <AdjustButton>
+                  <MoreVertIcon />
+                </AdjustButton>
+              </AdjustDate>
             </SettingDate>
           </Setting4>
           <Setting5>
-          <td>Alfreds Futterkiste</td>
-          <SettingDate>
-              <span>Maria Anders</span>
-              <span>Maria Anders</span>
-              <span>Maria Anders</span>
+            <SettingName>
+              วันเฉลิมพระชนมพรรษาสมเด็จพระนางเจ้าฯ พระบรมราชินี
+            </SettingName>
+            <SettingDate>
+              <AdjustDate>
+                <span>1</span>
+                <span>มกราคม</span>
+                <span>1 วัน </span>
+                <AdjustButton>
+                  <MoreVertIcon />
+                </AdjustButton>
+              </AdjustDate>
             </SettingDate>
           </Setting5>
           <Setting6>
-          <td>Alfreds Futterkiste</td>
-          <SettingDate>
-              <span>Maria Anders</span>
-              <span>Maria Anders</span>
-              <span>Maria Anders</span>
+            <SettingName> วันเข้าพรรษา</SettingName>
+            <SettingDate>
+              <AdjustDate>
+                <span>1</span>
+                <span>มกราคม</span>
+                <span>1 วัน </span>
+                <AdjustButton>
+                  <MoreVertIcon />
+                </AdjustButton>
+              </AdjustDate>
             </SettingDate>
           </Setting6>
+          <Setting7>
+            <SettingName> วันเฉลิมพระชนมพรรษา ร.10</SettingName>
+            <SettingDate>
+              <AdjustDate>
+                <span>1</span>
+                <span>มกราคม</span>
+                <span>1 วัน </span>
+                <AdjustButton>
+                  <MoreVertIcon />
+                </AdjustButton>
+              </AdjustDate>
+            </SettingDate>
+          </Setting7>
+          <Setting8>
+            <SettingName> วันแม่แห่งชาติ</SettingName>
+            <SettingDate>
+              <AdjustDate>
+                <span>1</span>
+                <span>มกราคม</span>
+                <span>1 วัน </span>
+                <AdjustButton>
+                  <MoreVertIcon />
+                </AdjustButton>
+              </AdjustDate>
+            </SettingDate>
+          </Setting8>
+          <Setting9>
+            <SettingName> วันคล้ายวันสวรรคต ร.9</SettingName>
+            <SettingDate>
+              <AdjustDate>
+                <span>1</span>
+                <span>มกราคม</span>
+                <span>1 วัน </span>
+                <AdjustButton>
+                  <MoreVertIcon />
+                </AdjustButton>
+              </AdjustDate>
+            </SettingDate>
+          </Setting9>
+          <Setting10>
+            <SettingName> วันปิยมหาราช</SettingName>
+            <SettingDate>
+              <AdjustDate>
+                <span>1</span>
+                <span>มกราคม</span>
+                <span>1 วัน </span>
+                <AdjustButton>
+                  <MoreVertIcon />
+                </AdjustButton>
+              </AdjustDate>
+            </SettingDate>
+          </Setting10>
+          <ButtonChange>
+            <ButtonSelect>เพิ่มวันหยุดใหม่ +</ButtonSelect>
+          </ButtonChange>
+          <AdvanceSelet>
+            <AdvanceButton>Advance</AdvanceButton>
+          </AdvanceSelet>
         </Table>
       </SettingTable>
     </div>
